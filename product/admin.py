6f0db676af1b5f886b818064347342e7e339ab79
Uluk <nelson.mandela.99.99@gmail.com>
2022-05-18 18:58:05 +0600

    model created

---
+++ product/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from .models import Category, Product
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    list_display_links = ('title', )
+    search_fields = ('title', )
+    prepopulated_fields = {'slug': ('title', )}
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'price', 'category', 'created_at',)
+    list_display_links = ('title', )
+    search_fields = ('title', )
+    prepopulated_fields = {'slug': ('title',  )}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
---
+++ product/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title', )
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def save(self):
+        self.slug = self.title.lower().replace(" ", '-')
+        return super().save()
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product')
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField(max_length=500)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='image')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title', )
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def save(self):
+        self.slug = self.title.lower().replace(" ", '-')
+        return super().save()
